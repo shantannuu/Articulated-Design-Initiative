06c859471fc40b4b0c2835362484d87b96f6dfc1
shantannuu <shantanubolate@gmail.com>
2024-01-30 18:44:35 +0530

    shan

--- frontend/src/Pages/Media/Media.js
+++ frontend/src/Pages/Media/Media.js
@@ -2,6 +2,10 @@ import React from 'react'
 import News from '../../Components/New2/News'
 import './Media.css'
 function Media() {
+    useEffect(() => {
+        usersVisited();
+        window.scrollTo(0, 0);
+    }, []);
     return (
         <div className='mediaPage'>
 
