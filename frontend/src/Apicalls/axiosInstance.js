0c8c40218ff357e5d15883b95f98cc443baa816c
shantannuu <shantanubolate@gmail.com>
2024-01-30 18:36:21 +0530

    shan

--- frontend/src/Apicalls/axiosInstance.js
+++ frontend/src/Apicalls/axiosInstance.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL:"http://172.20.10.4:5000",
+    // baseURL:"http://172.20.10.4:5000",
     // baseURL:"http://192.168.29.225:5000",
-    // baseURL:"http://localhost:5000",
+    baseURL:"http://localhost:5000",
     headers: {
         authorization: `Bearer ${localStorage.getItem('token')}`
     },
